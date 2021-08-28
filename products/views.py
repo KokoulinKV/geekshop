d137386a602ffb260f73530533107d9576cee7bd
KokoulinKV <jelikavar@gmail.com>
2021-08-28 18:17:37 +0700

    "load static" and test

--- products/views.py
+++ products/views.py
@@ -9,3 +9,23 @@ def index(request):
 
 def products(request):
     return render(request, 'products.html')
+
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'user': 'Ivan',
+        'description': 'Добро пожаловать в GeekShop!',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.'}
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.'}
+        ]
+    }
+
+    return render(request, 'test.html', context)
