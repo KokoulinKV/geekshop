d394139abeb36abd9e9c3a7d8f5460327f0439ae
KokoulinKV <jelikavar@gmail.com>
2021-08-29 02:33:50 +0700

    add datetime in header; add and parse "index.html"; add "|title" tag to titles.

--- products/views.py
+++ products/views.py
@@ -5,17 +5,15 @@ import json
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    data = 'products/templates/products/index.json'
+    with open(data, 'r', encoding='utf-8') as products_json:
+        context = json.load(products_json)
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
-
-
-def test(request):
-
     data = 'products/templates/products/products.json'
-    with open(data,'r', encoding='utf-8') as products_json:
+    with open(data, 'r', encoding='utf-8') as products_json:
         context = json.load(products_json)
 
-    return render(request, 'products/test.html', context)
+    return render(request, 'products/products.html', context)
