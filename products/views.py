54e26bad4510106f6b33b8ae4964aedc91b4e279
KokoulinKV <jelikavar@gmail.com>
2021-09-02 15:45:04 +0700

    upload data from db to html, remove index.json

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import json
 import os
+from products.models import  Product, ProductCategory
 
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -8,15 +9,16 @@ MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
-    data = os.path.join(MODULE_DIR, 'fixtures/index.json')
-    with open(data, 'r', encoding='utf-8') as products_json:
-        context = json.load(products_json)
-    return render(request, 'products/index.html', context)
-
+    return render(request, 'products/index.html')
 
 def products(request):
-    data = os.path.join(MODULE_DIR, 'fixtures/products.json')
-    with open(data, 'r', encoding='utf-8') as products_json:
-        context = json.load(products_json)
-
-    return render(request, 'products/products.html', context)
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()
+    return render(request, 'products/products.html', locals())
+#
+# def products(request):
+#     data = os.path.join(MODULE_DIR, 'fixtures/products.json')
+#     with open(data, 'r', encoding='utf-8') as products_json:
+#         context = json.load(products_json)
+#
+#     return render(request, 'products/products.html', context)
