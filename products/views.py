eec426c312fe4103b6d753363223941f2d33d9fd
KokoulinKV <jelikavar@gmail.com>
2021-09-02 12:31:21 +0700

    alter json path

--- products/views.py
+++ products/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 import json
+import os
 
+
+MODULE_DIR = os.path.dirname(__file__)
 # Create your views here.
 
 
 def index(request):
-    data = 'products/templates/products/index.json'
+    data = os.path.join(MODULE_DIR, 'fixtures/index.json')
     with open(data, 'r', encoding='utf-8') as products_json:
         context = json.load(products_json)
     return render(request, 'products/index.html', context)
 
 
 def products(request):
-    data = 'products/templates/products/products.json'
+    data = os.path.join(MODULE_DIR, 'fixtures/products.json')
     with open(data, 'r', encoding='utf-8') as products_json:
         context = json.load(products_json)
 
