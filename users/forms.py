f480ac70b20d984f3811c509537a3859f0e79391
KokoulinKV <jelikavar@gmail.com>
2021-09-06 18:06:38 +0700

    create forms.py and update views.py

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Enter name'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Enter password'}))
+
+    class Meta:
+        model = User
+        fields = ('password', 'username')
--- users/views.py
+++ users/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm
+
 
 # Create your views here.
 
 
 def login(request):
-    context = {'title':'Login'}
-    return render(request, 'users/login.html', context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+
+    else:
+        form = UserLoginForm()
+        context = {'title': 'Login',
+                       'form': form}
+        return render(request, 'users/login.html', context)
+
 
 def registration(request):
-    context = {'title': 'Create Account'}
+    form = UserLoginForm(data=request.POST)
+    context = {'title': 'Create Account',
+               'form': form}
     return render(request, 'users/register.html', context)
