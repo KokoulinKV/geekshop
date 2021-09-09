cb80e37aeb27db2db9af194ae687eeeb0e2338b6
KokoulinKV <jelikavar@gmail.com>
2021-09-09 20:45:38 +0700

    added window with errors or messages

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm, UserRegistrationFrom
 
@@ -17,9 +17,6 @@ def login(request):
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-        else:
-            print(form.errors)
-
     else:
         form = UserLoginForm()
     context = {'title': 'Login',
@@ -32,10 +29,8 @@ def registration(request):
         form = UserRegistrationFrom(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registration successful!')
             return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print(form.errors)
-
     else:
         form = UserRegistrationFrom()
     context = {'title': 'Create Account',
