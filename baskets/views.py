76f82514809c0516ed07f9f0c262c7b99383cabf
KokoulinKV <jelikavar@gmail.com>
2021-09-09 22:58:40 +0700

    added controller and urls

--- baskets/views.py
+++ baskets/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from products.models import Product
+from baskets.models import Basket
 
 # Create your views here.
+def basket_add(request, id):
+    product = Product.objects.get(id=id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        baskets = baskets.first()
+        baskets.quantity += 1
+        return  HttpResponseRedirect(request.META.get('HTTP_REFERER'))
