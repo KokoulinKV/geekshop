6cc23c4c16e88ceb8ea1d34936d0c80efdc1ab49
KokoulinKV <jelikavar@gmail.com>
2021-09-15 00:39:47 +0700

    added app admins and templates

--- admins/urls.py
+++ admins/urls.py
@@ -13,21 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-from products.views import index
+from users.views import login,registration, logout, profile
+
+app_name = 'admins'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users')),
-    path('baskets/', include('baskets.urls', namespace='baskets')),
-    path('admins/', include('admins.urls', namespace='admins'))
+    path('login/', login, name='login'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
