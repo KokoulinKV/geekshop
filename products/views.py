3a908f1719b4a94009389d36c2cd56aa7ae7d370
KokoulinKV <jelikavar@gmail.com>
2021-09-21 21:33:23 +0700

    add filter by products categories

--- products/views.py
+++ products/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 import os
-from products.models import  Product, ProductCategory
-
+from products.models import Product, ProductCategory
 
 MODULE_DIR = os.path.dirname(__file__)
+
+
 # Create your views here.
 
 
 def index(request):
-    context = {'title':'GeekShop'}
+    context = {'title': 'GeekShop'}
     return render(request, 'products/index.html', context)
 
-def products(request):
-    context = {'title':'GeekShop - Catalog'}
-    context['categories'] = ProductCategory.objects.all()
-    context['products'] = Product.objects.all()
-    return render(request, 'products/products.html', context)
 
+def products(request, id=None):
+    if id != None:
+        products_filter = Product.objects.filter(category_id=id)
+
+    else:
+        products_filter = Product.objects.all()
+    context = {
+        'title': 'GeekShop - Catalog',
+        'categories': ProductCategory.objects.all()
+    }
+    context['products'] = products_filter
+    return render(request, 'products/products.html', context)
