25ba8a8bd9c7f0778f410b4b62279eeeaabd5428
KokoulinKV <jelikavar@gmail.com>
2021-10-03 12:56:47 +0700

    added userprofile model and migrated it

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
@@ -12,10 +14,31 @@ class User(AbstractUser):
 
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
     # activation_key_expires = models.DateTimeField(default=(now()+timedelta(hours=48)))
 
-
     def is_activation_key_expires(self):
-        if now() <=self.activation_key_expires + timedelta(hours=48):
+        if now() <= self.activation_key_expires + timedelta(hours=48):
             return False
         return True
+
+
+
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOICES = (
+        (MALE, 'M'),
+        (FEMALE, 'W')
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='tags', max_length=128, blank=True)
+    about_me = models.TextField(verbose_name='about', blank=True)
+    gender = models.CharField(verbose_name='gender', choices=GENDER_CHOICES, blank=True, max_length=3)
+
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
