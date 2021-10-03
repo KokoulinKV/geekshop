d78885bd1dcad938b428e6c881128478361cf1b1
KokoulinKV <jelikavar@gmail.com>
2021-10-03 13:50:54 +0700

    added profiles to new users (registration)

--- users/models.py
+++ users/models.py
@@ -38,6 +38,11 @@ class UserProfile(models.Model):
     about_me = models.TextField(verbose_name='about', blank=True)
     gender = models.CharField(verbose_name='gender', choices=GENDER_CHOICES, blank=True, max_length=3)
 
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+           UserProfile.objects.create(user=instance)
+
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
