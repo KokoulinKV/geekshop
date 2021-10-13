d6a62a8c4b986ea531d15e967a63e1a430ae50d0
KokoulinKV <jelikavar@gmail.com>
2021-10-13 22:28:49 +0700

    publication

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'GeekShop',
+        'NAME': 'geekshop',
         'USER':'postgres',
     }
 }
