679997283f47a1bb42f0ad853c10cea9fff097a6
KokoulinKV <jelikavar@gmail.com>
2021-10-24 13:50:58 +0700

    add cache to basket

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -94,7 +94,7 @@ class OrderCreate(CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
-                basket_items.delete()
+                # basket_items.delete()
             else:
                 formset = OrderFormSet()
 
